9d1a2e631b24fa84d190c8aeb940aa387e3d17bd
JihooRyu00 <harryryujh@naver.com>
2026-07-14 10:31:37 +0900

    homework feedback

--- if_else_exam/src/if_else_exam/Problem2.java
+++ if_else_exam/src/if_else_exam/Problem2.java
@@ -8,6 +8,8 @@ public class Problem2 {
 		int daughter = 11;
 		int adultOneWayFlightFare = 300_000;
 		int kidOneWayFlightFare = 120_000;
+
+		final int ADULT_AGE = 19;
 		// 3인 가족이 100만원으로 비행기를 타고 편도 여행을 가려합니다.
 		// 부모님의 나이는 각각 40, 36세입니다.
 		// 딸의 나이는 11세입니다.
@@ -17,9 +19,9 @@ public class Problem2 {
 		// 여행을 떠날 수 있다면 "여행가자!"
 		// 여행을 떠날 수 없다면 "다음에가자"
 		// 를 출력해보세요.
-//		int totalFee = (mother >= 19) ? adultOneWayFlightFare : kidOneWayFlightFare;
-//		totalFee += (father >= 19) ? adultOneWayFlightFare : kidOneWayFlightFare;
-//		totalFee += (daughter >= 19) ? adultOneWayFlightFare : kidOneWayFlightFare;
+//		int totalFee = (mother >= ADULT_AGE) ? adultOneWayFlightFare : kidOneWayFlightFare;
+//		totalFee += (father >= ADULT_AGE) ? adultOneWayFlightFare : kidOneWayFlightFare;
+//		totalFee += (daughter >= ADULT_AGE) ? adultOneWayFlightFare : kidOneWayFlightFare;
 //		if (totalFee<=money) {
 //			System.out.println("여행가자!");
 //		}
@@ -28,14 +30,25 @@ public class Problem2 {
 //		}
 		int numAdult = 0;
 		int numKid = 0;
-		if (mother >= 19) numAdult++;
-		else numKid++;
-		if (father >= 19) numAdult++;
-		else numKid++;
-		if (daughter >= 19) numAdult++;
-		else numKid++;
-		
+
+		if (mother >= ADULT_AGE) {
+			numAdult++;
+		} else {
+			numKid++;
+		}
+		if (father >= ADULT_AGE) {
+			numAdult++;
+		} else {
+			numKid++;
+		}
+		if (daughter >= ADULT_AGE) {
+			numAdult++;
+		} else {
+			numKid++;
+		}
+
 		int totalFee = adultOneWayFlightFare * numAdult + kidOneWayFlightFare * numKid;
+
 		if (totalFee <= money) {
 			System.out.println("여행가자!");
 		} else {
