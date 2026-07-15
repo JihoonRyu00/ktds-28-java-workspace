5db90940c8d1d84eea4a10a21d194512738f67d5
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2026-07-15 20:07:52 +0900

    fix floating point issue

--- workflow/src/workflow/MethodHW.java
+++ workflow/src/workflow/MethodHW.java
@@ -49,7 +49,7 @@ public class MethodHW {
         sieve[0] = false;
         sieve[1] = false;
 
-        for (int curr = 2; curr <= Math.sqrt(num); ++curr) {
+        for (int curr = 2; curr * curr <= num; ++curr) {
             if (sieve[curr]) {
                 for (int i = curr * curr; i <= num; i += curr) {
                     sieve[i] = false;
