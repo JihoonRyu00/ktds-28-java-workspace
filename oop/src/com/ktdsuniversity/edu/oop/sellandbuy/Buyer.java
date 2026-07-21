b5a27d2f3170226577f464685c4017e26187b55c
JihoonRyu00 <harryryujh@naver.com>
2026-07-22 00:22:33 +0900

    seller and buyer class constructor hw

--- oop/src/com/ktdsuniversity/edu/oop/sellandbuy/Buyer.java
+++ oop/src/com/ktdsuniversity/edu/oop/sellandbuy/Buyer.java
@@ -1,5 +1,71 @@
 package com.ktdsuniversity.edu.oop.sellandbuy;
 
+/**
+ * 구매자 클래스
+ */
 public class Buyer {
+
+	/**
+	 * 구매자 잔고
+	 */
+	private int balance;
+
+	/**
+	 * 구매자 장바구니
+	 */
+	private int cart;
+
+	/**
+	 * 구매자 생성자
+	 * 
+	 * @param balance
+	 * @param cart
+	 */
+	Buyer(int balance, int cart) {
+		this.balance = balance;
+		this.cart = cart;
+	}
+
+	/**
+	 * 구매자 잔고 getter
+	 * 
+	 * @return
+	 */
+	public int getBalance() {
+		return this.balance;
+	}
+
+	/**
+	 * 구매자 장바구니 getter
+	 * 
+	 * @return
+	 */
+	public int getCart() {
+		return this.cart;
+	}
+
+	/**
+	 * 구매
+	 * 
+	 * @param seller
+	 * @param quantity
+	 */
+	public void buy(Seller seller, int quantity) {
+		int totalPrice = seller.getPrice() * quantity;
+		if (totalPrice > this.balance) {
+			return;
+		}
+		if (quantity > seller.getStock()) {
+			quantity = seller.getStock();
+		}
+		this.cart += quantity;
+		this.balance -= seller.sell(quantity);
+	}
+
+	public static void main(String[] args) {
+		Buyer buyer = new Buyer(10000, 0);
+		Seller seller = new Seller(10, 2000, 0);
+		buyer.buy(seller, 3);
+	}
 
 }
--- oop/src/com/ktdsuniversity/edu/oop/sellandbuy/Seller.java
+++ oop/src/com/ktdsuniversity/edu/oop/sellandbuy/Seller.java
@@ -1,5 +1,66 @@
 package com.ktdsuniversity.edu.oop.sellandbuy;
 
+/**
+ * 판매자 클래스
+ */
 public class Seller {
 
+	/**
+	 * 판매자 상품 재고
+	 */
+	private int stock;
+
+	/**
+	 * 판매자 상품 가격
+	 */
+	private int price;
+
+	/**
+	 * 판매자 매출액
+	 */
+	private int totalIncome;
+
+	/**
+	 * 판매자 생성자
+	 * 
+	 * @param stock
+	 * @param price
+	 * @param totalIncome
+	 */
+	Seller(int stock, int price, int totalIncome) {
+		this.stock = stock;
+		this.price = price;
+		this.totalIncome = totalIncome;
+	}
+
+	/**
+	 * 판매자 상품 재고 getter
+	 * 
+	 * @return
+	 */
+	public int getStock() {
+		return this.stock;
+	}
+
+	/**
+	 * 판매자 상품 가격 getter
+	 * 
+	 * @return
+	 */
+	public int getPrice() {
+		return this.price;
+	}
+
+	/**
+	 * 판매
+	 * 
+	 * @param quantity
+	 * @return
+	 */
+	public int sell(int quantity) {
+		this.stock -= quantity;
+		int totalPrice = quantity * this.price;
+		this.totalIncome += totalPrice;
+		return totalPrice;
+	}
 }
