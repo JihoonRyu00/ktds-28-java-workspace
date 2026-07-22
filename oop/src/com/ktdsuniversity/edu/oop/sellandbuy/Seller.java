84f202e297f1b8451787a9c15dcf9f162889a00c
JihooRyu00 <harryryujh@naver.com>
2026-07-22 09:34:52 +0900

    add salesCount member field and printStatus method

--- oop/src/com/ktdsuniversity/edu/oop/sellandbuy/Seller.java
+++ oop/src/com/ktdsuniversity/edu/oop/sellandbuy/Seller.java
@@ -5,19 +5,16 @@ package com.ktdsuniversity.edu.oop.sellandbuy;
  */
 public class Seller {
 
-	/**
-	 * 판매자 상품 재고
-	 */
+	/** 판매자 상품 재고 */
 	private int stock;
 
-	/**
-	 * 판매자 상품 가격
-	 */
+	/** 판매자 상품 가격 */
 	private int price;
 
-	/**
-	 * 판매자 매출액
-	 */
+	/** 총 판매 수량 */
+	private int salesCount;
+
+	/** 판매자 매출액 */
 	private int totalIncome;
 
 	/**
@@ -26,11 +23,13 @@ public class Seller {
 	 * @param stock
 	 * @param price
 	 * @param totalIncome
+	 * @param salesCount
 	 */
-	Seller(int stock, int price, int totalIncome) {
+	Seller(int stock, int price, int totalIncome, int salesCount) {
 		this.stock = stock;
 		this.price = price;
 		this.totalIncome = totalIncome;
+		this.salesCount = salesCount;
 	}
 
 	/**
@@ -59,8 +58,20 @@ public class Seller {
 	 */
 	public int sell(int quantity) {
 		this.stock -= quantity;
+		this.salesCount += quantity;
 		int totalPrice = quantity * this.price;
 		this.totalIncome += totalPrice;
 		return totalPrice;
+	}
+
+	/**
+	 * 판매자 상태 출력
+	 */
+	public void printStatus() {
+		System.out.println("===== 판매자의 정보 =====");
+		System.out.println("현재 재고\t: " + this.stock);
+		System.out.println("판매 상품 단가\t: " + this.price);
+		System.out.println("총 판매 수량\t: " + this.salesCount);
+		System.out.println("총 매출액\t: " + this.totalIncome);
 	}
 }
