c7128486d9de39e6d8bd2d62f7cc78ca9f4b9f5e
JihooRyu00 <harryryujh@naver.com>
2026-07-23 10:13:31 +0900

    comment unrunnable code

--- oop/src/com/ktdsuniversity/edu/oop/exam/StudentTest.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/StudentTest.java
@@ -4,7 +4,7 @@ public class StudentTest {
 
 	public static void main(String[] args) {
 		Student student = new Student(0, 0, 0, 0);
-		student.getSumAllScores(); // private이라 접근 불가
-		System.out.println(student.java); // private이라 접근 불가
+//		student.getSumAllScores(); // private이라 접근 불가
+//		System.out.println(student.java); // private이라 접근 불가
 	}
 }
