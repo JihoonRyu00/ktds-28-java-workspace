7133fb05402c334a944b49bb633bf51373dd3422
JihooRyu00 <harryryujh@naver.com>
2026-07-23 10:36:17 +0900

    class constant practice

---
+++ oop/src/com/ktdsuniversity/edu/oop/exam/statics/MathCalculator.java
@@ -0,0 +1,54 @@
+package com.ktdsuniversity.edu.oop.exam.statics;
+
+/**
+ * 사칙연산기
+ */
+public class MathCalculator {
+
+	/** 첫번째 피연산자 */
+	private int firstOperand;
+	/** 두번째 피연산자 */
+	private int secondOperand;
+
+	/**
+	 * 
+	 * @param firstNum
+	 * @param secondNum
+	 */
+	public MathCalculator(int firstOperand, int secondOperand) {
+		this.firstOperand = firstOperand;
+		this.secondOperand = secondOperand;
+	}
+
+	/**
+	 * 두 수를 연산하여 반환
+	 * 
+	 * @see com.ktdsuniversity.edu.oop.exam.statics.Operators 연산자 종류
+	 * @param operator 연산 종류 (0: 더하기, 1: 빼기, 2: 곱하기, 3: 나누기 몫, 4: 나누기 나머지)
+	 * @return
+	 */
+	public int getCoumputeResult(int operator) {
+		if (operator == Operators.ADD) {
+			return this.firstOperand + this.secondOperand;
+		} else if (operator == Operators.SUB) {
+			return this.firstOperand - this.secondOperand;
+		} else if (operator == Operators.MUL) {
+			return this.firstOperand * this.secondOperand;
+		} else if (operator == Operators.DIV) {
+			return this.firstOperand / this.secondOperand;
+		} else if (operator == Operators.MOD) {
+			return this.firstOperand % this.secondOperand;
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+		MathCalculator mc = new MathCalculator(10, 2);
+		System.out.println(mc.getCoumputeResult(Operators.ADD));
+		System.out.println(mc.getCoumputeResult(Operators.SUB));
+		System.out.println(mc.getCoumputeResult(Operators.MUL));
+		System.out.println(mc.getCoumputeResult(Operators.DIV));
+		System.out.println(mc.getCoumputeResult(Operators.MOD));
+	}
+}
