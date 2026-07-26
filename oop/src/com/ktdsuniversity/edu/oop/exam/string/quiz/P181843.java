56da4a02c0053596f0e5c6f0e46d8b03e679acb6
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 14:11:46 +0900

    P181843

--- oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181843.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181843.java
@@ -3,4 +3,16 @@ package com.ktdsuniversity.edu.oop.exam.string.quiz;
 //https://school.programmers.co.kr/learn/courses/30/lessons/181843
 public class P181843 {
 
+    public static int solution(String my_string, String target) {
+		if (my_string.contains(target)) {
+			return 1;
+		}
+		return 0;
+    }
+    
+    public static void main(String[] args) {
+		System.out.println(solution("banana", "ana"));
+
+		System.out.println(solution("banana", "wxyz"));
+	}
 }
