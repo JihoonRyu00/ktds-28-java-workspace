fbcaf2e34f529e094ab8d2cb6297b6d2f0a5d37a
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 14:13:58 +0900

    P181845

--- oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181845.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181845.java
@@ -3,4 +3,13 @@ package com.ktdsuniversity.edu.oop.exam.string.quiz;
 //https://school.programmers.co.kr/learn/courses/30/lessons/181845
 public class P181845 {
 
+	public static String solution(int n) {
+		return n + "";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution(123));
+
+		System.out.println(solution(2573));
+	}
 }
