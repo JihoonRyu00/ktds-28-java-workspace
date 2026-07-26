2231fa53400029708d089d03b23d26863f8d5f54
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 14:43:20 +0900

    P181875

--- oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181875.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181875.java
@@ -3,4 +3,27 @@ package com.ktdsuniversity.edu.oop.exam.string.quiz;
 //https://school.programmers.co.kr/learn/courses/30/lessons/181875
 public class P181875 {
 
+	public static String[] solution(String[] strArr) {
+		String[] newStrArr = strArr.clone();
+		for (int i = 0; i < newStrArr.length; ++i) {
+			if (i % 2 == 0) {
+				newStrArr[i] = newStrArr[i].toLowerCase();
+			} else {
+				newStrArr[i] = newStrArr[i].toUpperCase();
+			}
+		}
+		return newStrArr;
+	}
+
+	public static void main(String[] args) {
+		String[] strArr1 = solution(new String[] { "AAA", "BBB", "CCC", "DDD" });
+		for (String str : strArr1) {
+			System.out.println(str);
+		}
+
+		String[] strArr2 = solution(new String[] { "aaa", "BBB", "ccc", "DDD" });
+		for (String str : strArr2) {
+			System.out.println(str);
+		}
+	}
 }
