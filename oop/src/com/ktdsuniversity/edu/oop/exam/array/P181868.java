034112eb8db66699311ae6ac9789fb72f25ceae0
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 19:56:27 +0900

    P181868

--- oop/src/com/ktdsuniversity/edu/oop/exam/array/P181868.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/array/P181868.java
@@ -1,5 +1,20 @@
 package com.ktdsuniversity.edu.oop.exam.array;
 
+//https://school.programmers.co.kr/learn/courses/30/lessons/181868
 public class P181868 {
 
+	public static String[] solution(String my_string) {
+		String[] answer = my_string.strip().split("[\\s]+");
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		for (String s : solution(" i    love  you")) {
+			System.out.print(s + " ");
+		}
+
+		for (String s : solution("    programmers  ")) {
+			System.out.print(s + " ");
+		}
+	}
 }
