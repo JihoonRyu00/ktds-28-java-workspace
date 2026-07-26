4c8cbe48d5036cfbbfab12032878cf0437ba58ef
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 14:22:16 +0900

    P181847

--- oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181847.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181847.java
@@ -3,4 +3,20 @@ package com.ktdsuniversity.edu.oop.exam.string.quiz;
 //https://school.programmers.co.kr/learn/courses/30/lessons/181847
 public class P181847 {
 
+	public static String solution(String n_str) {
+		int curr = 0;
+		while (curr <= n_str.length() - 1 && n_str.toCharArray()[curr] == '0') {
+			curr++;
+		}
+		if (curr > n_str.length() - 1) {
+			return "";
+		}
+		return n_str.substring(curr);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution("0010"));
+
+		System.out.println(solution("854020"));
+	}
 }
