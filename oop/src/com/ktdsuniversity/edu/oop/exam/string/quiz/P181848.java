2c5ce6d3fa200145b8c796e9d3779c695e4ab3b2
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 14:26:31 +0900

    P181848

--- oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181848.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181848.java
@@ -3,4 +3,13 @@ package com.ktdsuniversity.edu.oop.exam.string.quiz;
 //https://school.programmers.co.kr/learn/courses/30/lessons/181848
 public class P181848 {
 
+	public static int solution(String n_str) {
+		return Integer.parseInt(n_str);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution("10"));
+
+		System.out.println(solution("8542"));
+	}
 }
