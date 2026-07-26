e5e19a0631c807c4d76a0e6969c8593c798361f5
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 14:48:40 +0900

    P181878

--- oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181878.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181878.java
@@ -3,4 +3,16 @@ package com.ktdsuniversity.edu.oop.exam.string.quiz;
 //https://school.programmers.co.kr/learn/courses/30/lessons/181878
 public class P181878 {
 
+	public static int solution(String myString, String pat) {
+		if (myString.toLowerCase().contains(pat.toLowerCase())) {
+			return 1;
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution("AbCdEfG", "aBc"));
+
+		System.out.println(solution("aaAA", "aaaaa"));
+	}
 }
