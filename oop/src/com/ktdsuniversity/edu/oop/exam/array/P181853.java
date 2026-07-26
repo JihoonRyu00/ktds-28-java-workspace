f8ce9405294664111ac3dfeefa110c8fa6dd6b90
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 19:01:30 +0900

    P181854

--- oop/src/com/ktdsuniversity/edu/oop/exam/array/P181853.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/array/P181853.java
@@ -1,6 +1,21 @@
 package com.ktdsuniversity.edu.oop.exam.array;
 
+import java.util.Arrays;
+
 //https://school.programmers.co.kr/learn/courses/30/lessons/181853
 public class P181853 {
 
+	public static int[] solution(int[] num_list) {
+		Arrays.sort(num_list);
+		int[] resultArr = new int[5];
+		System.arraycopy(num_list, 0, resultArr, 0, 5);
+		return resultArr;
+	}
+
+	public static void main(String[] args) {
+		int[] resultArr = solution(new int[] { 12, 4, 15, 46, 38, 1, 14 });
+		for (int a : resultArr) {
+			System.out.println(a);
+		}
+	}
 }
--- oop/src/com/ktdsuniversity/edu/oop/exam/array/P181854.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/array/P181854.java
@@ -3,4 +3,30 @@ package com.ktdsuniversity.edu.oop.exam.array;
 //https://school.programmers.co.kr/learn/courses/30/lessons/181854
 public class P181854 {
 
+	public static int[] solution(int[] arr, int n) {
+		int arrLength = arr.length;
+		int[] answer = new int[arrLength];
+		for (int i = 0; i < arrLength; ++i) {
+			if (i % 2 == arrLength % 2) {
+				answer[i] = arr[i];
+			} else {
+				answer[i] = arr[i] + n;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] arr1 = { 49, 12, 100, 276, 33 };
+		int[] resultArr1 = solution(arr1, 27);
+		for (int a : resultArr1) {
+			System.out.print(a + " ");
+		}
+
+		int[] arr2 = { 444, 555, 666, 777 };
+		int[] resultArr2 = solution(arr2, 100);
+		for (int a : resultArr2) {
+			System.out.print(a + " ");
+		}
+	}
 }
