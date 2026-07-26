4c0b0ef7ff52c197ebf1d65ab1bf461d1c1f508a
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 16:42:11 +0900

    P181941

--- oop/src/com/ktdsuniversity/edu/oop/exam/array/P181941.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/array/P181941.java
@@ -3,4 +3,16 @@ package com.ktdsuniversity.edu.oop.exam.array;
 // https://school.programmers.co.kr/learn/courses/30/lessons/181941
 public class P181941 {
 
+	public static String solution(String[] arr) {
+		StringBuffer sb = new StringBuffer();
+		for (String str : arr) {
+			sb.append(str);
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		String[] arr = { "a", "b", "c" };
+		System.out.println(solution(arr));
+	}
 }
