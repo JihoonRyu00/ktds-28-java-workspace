e5c9f28a2b17dad3d9e9eb3f14743330478ec151
JihoonRyu00 <harryryujh@naver.com>
2026-07-26 14:45:37 +0900

    P181876

--- oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181876.java
+++ oop/src/com/ktdsuniversity/edu/oop/exam/string/quiz/P181876.java
@@ -3,4 +3,13 @@ package com.ktdsuniversity.edu.oop.exam.string.quiz;
 //https://school.programmers.co.kr/learn/courses/30/lessons/181876
 public class P181876 {
 
+	public static String solution(String myString) {
+		return myString.toLowerCase();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution("aBcDeFg"));
+
+		System.out.println(solution("aaa"));
+	}
 }
